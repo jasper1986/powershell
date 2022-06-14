f91ec51be8bc09cd23d24a2ae65a6ccb3b7821db
jasper1986 <40769382+jasper1986@users.noreply.github.com>
2022-06-14 20:51:30 +0200

    Create ShartePoint-Site-maken.ps1

---
+++ ShartePoint-Site-maken.ps1
@@ -0,0 +1,20 @@
+﻿# Parameters
+$module = 'Microsoft.Online.SharePoint.PowerShell'
+$adminURL = 'https://jportalnl-admin.sharepoint.com//'
+$siteURL = 'https://jportalnl.sharepoint.com/sites/communications'
+$siteTitle = 'Communicatie site'
+$siteOwner = 'info@jportal.nl'
+$template = 'SITEPAGEPUBLISHING#0'
+$quota = 2048
+
+# Installeer SharePoint Online module
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell
+
+# Update SharePoint Online module
+Update-Module -Name Microsoft.Online.SharePoint.PowerShell
+
+# Connectie naar SharePoint Online
+Connect-SPOService -Url $adminURL
+ 
+# Maak een nieuwe communication site
+New-SPOSite -Url $siteURL -Owner $siteOwner -Template $template -StorageQuota $quota -Title $siteTitle
